a13cd79c16b9af7a5275bdd217730148afebbcee
Gal Levy <levigal50@gmail.com>
2025-07-28 17:03:23 +0300

    Add React Router with Home and About pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
 import './styles/App.css'
-import UsersTable from './components/UsersTable'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <>
-     <UsersTable />
-    </>
-  )
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
